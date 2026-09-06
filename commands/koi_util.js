ebd1fcd54b3056fc3021443dff33220536dc990a
hallow_303 <adailtonsouza@acad.ifma.edu.br>
2026-09-06 12:17:22 -0300

    Delete commands directory
    Para a atualização no bot, pq estava dando muitos erros por contar da quantidade de arquivos que estavam sendo carregados pelo Index.js

--- commands/koi_util.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    app.command("/koi-calc", async({command, ack, respond}) =>{
-        await ack();
-        const calc = command.text;
-        let resultado = eval(calc);
-        await respond({text:`Resultado: ${resultado}`});
-    });
-}
